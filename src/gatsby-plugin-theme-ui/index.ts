832fe37d9bba368d2028ac0b598328ce490ea1c1
Jinwoo Oh <arkist@gmail.com>
2021-07-27 16:26:49 -0700

    Change primary color

--- src/gatsby-plugin-theme-ui/index.ts
+++ src/gatsby-plugin-theme-ui/index.ts
@@ -1,4 +1,12 @@
-import theme from '@lekoarts/gatsby-theme-minimal-blog/src/gatsby-plugin-theme-ui/index'
+import minimalBlogTheme from '@lekoarts/gatsby-theme-minimal-blog/src/gatsby-plugin-theme-ui/index'
+import { merge } from 'theme-ui'
+import { tailwind } from '@theme-ui/presets'
+
+const theme = merge(minimalBlogTheme, {
+  colors: {
+    primary: tailwind.colors.blue[6],
+  }
+})
 
 export default {
   ...theme,
