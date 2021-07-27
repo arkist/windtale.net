e6fffd4bc728041cd004bbc77f4e895e4122bea7
Jinwoo Oh <arkist@gmail.com>
2021-07-27 16:57:44 -0700

    Updates

---
+++ component---cache-caches-gatsby-plugin-offline-app-shell-js-b9eea2dab113026b1bac.js
@@ -0,0 +1,2 @@
+(self.webpackChunkwindtale=self.webpackChunkwindtale||[]).push([[306],{91930:function(e,t,n){"use strict";var u=n(95318);t.__esModule=!0,t.default=void 0;var a=u(n(85354)),l=u(n(67294)),r=function(e){function t(){return e.apply(this,arguments)||this}return(0,a.default)(t,e),t.prototype.render=function(){return l.default.createElement(l.default.Fragment,null)},t}(l.default.Component);t.default=r}}]);
+//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-b9eea2dab113026b1bac.js.map
--- src/disqus/index.ts
+++
@@ -1,1 +0,0 @@
-export { Disqus } from './Disqus'
